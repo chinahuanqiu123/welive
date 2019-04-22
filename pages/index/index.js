1a66685c061ba40425c0d485fee88471d8e6f323
e <chinahuanqiu@163.com>
2019-04-22 22:34:58 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,100 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    role: true,
+    no:'',
+    college_list:[],
+    password:'',
+    college_id:1,
+    
+  },
+  onroleChange(event) {
+    var that=this;
+    const detail = event.detail;
+    this.setData({
+      role: !that.data.role
+    })
+
+  },
+  bindcollegeChange(e) {
+    var that=this;
+    const val = e.detail.value
+    this.setData({
+     college_id:that.data.college_list[val]['id']
+    })
+  },
+  handleloginClick:function(){
+    var that=this;
+    wx.request({
+      url: 'http://exam.alivefun.cn/live/getmysection', // 仅为示例，并非真实的接口地址
+      data: {
+        sid: that.data.no,
+        password:that.data.password,
+        role:that.data.role?'student':'teacher',
+        collegeid:that.data.college_id
+      },
+      method:'POST',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        
+        wx.setStorageSync('userinfo',res.data.student_info)
+            
+              if (that.data.role) {
+
+                wx.navigateTo({
+                  url: '/pages/student/index',
+                })
+              }
+              else {
+                wx.navigateTo({
+                  url: '/pages/teacher/index',
+                })
+              }
+           
+        
+       
+      }
+    })
+
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    var that=this;
+    wx.request({
+      url: 'http://exam.alivefun.cn/college/show/', // 仅为示例，并非真实的接口地址
+      type:'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        that.setData({
+          college_list:res.data.college_list
+        })
+      }
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  
+  
+})
